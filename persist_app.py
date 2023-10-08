cabc6a693cd97733a2021bf3e0c4a100b23d8866
priyansh4320 <priyanshudeshmukh4@gmail.com>
2023-10-08 13:24:20 +0530

    Revert "major code changes (api)"
    This reverts commit f2e6696925af2fa65d05d0e02e32d8b9f40ed760.

--- persist_app.py
+++ persist_app.py
@@ -10,6 +10,14 @@ from PIL import Image
 from facedetector import FaceDetector
 
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException , NoSuchWindowException , WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 
 import base64
 from io import BytesIO
@@ -20,31 +28,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 ##-------------------------------------------
-def rev_search(img_url):
-    url = "https://reverse-image-search-by-copyseeker.p.rapidapi.com/"
 
-    querystring = {"imageUrl":f"{img_url}"}
-
-    headers = {
-        "X-RapidAPI-Key": "19f351f953mshe8f8e82d609061bp1ff0a1jsn8affa0ab462b",
-        "X-RapidAPI-Host": "reverse-image-search-by-copyseeker.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-
-    if response.status_code == 200:
-        try:
-            result = response.json()
-            return result
-        except requests.exceptions.JSONDecodeError:
-            print("Failed to decode JSON. Response content:")
-            print(response.content)
-            return None
-    else:
-        st.write("Error:", response.status_code)
-        st.write("Response content:")
-        st.write(response.content)
-        return None
 
 ## define face extraction--------------------------------------------------------------------------
 
@@ -96,6 +80,99 @@ def face_extraction(uploaded_video):
             cropped = img.crop((left, top, right, bottom))
             extracted_face.append(cropped)
         return extracted_face
+    pass
+## define pimeyes----------------------------------------------------------------------------------
+def pimeye():
+    def installff():
+        os.system('sbase install geckodriver')
+        os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+    _ = installff()
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    driver = webdriver.Firefox(options=opts)
+    # Open the website
+    driver.get("https://pimeyes.com/en/login")  # Replace with the URL of the website you want to login to
+
+    # Find the login form elements (e.g., username and password fields, su
+    #login
+    type_attribute_value_pass = "password" 
+    type_attribute_value_user = "email" 
+    username = driver.find_element(By.XPATH, f'//*[@type="{type_attribute_value_user}"]')  # Replace "username" with the actual ID of the username field
+    password = driver.find_element(By.XPATH, f'//*[@type="{type_attribute_value_pass}"]')  # Replace "password" with the actual ID of the password field
+    submit_button = driver.find_element(By.CLASS_NAME, "login-btn")  # Replace "login-button" with the actual ID of the submit button
+
+    # Enter your login credentials
+    username.send_keys("aimagic123@mailinator.com")
+    password.send_keys("Aiismagic123!")
+    driver.find_element(By.XPATH,f'//input[@name="remember"]').click()
+    submit_button.click()
+    #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"icon notranslate")))
+    #open homepage
+    home_attribute = "Open mainpage"
+
+    homepage = driver.find_element(By.XPATH, f'//*[@aria-label="{home_attribute}"]')
+    link_url = homepage.get_attribute("href")
+    driver.execute_script(f'window.open("{link_url}");')
+    driver.switch_to.window(driver.window_handles[-1])
+
+
+    #upload faces
+    WebDriverWait(driver,20).until(
+        EC.visibility_of_element_located((By.CLASS_NAME,"understand"))
+    )
+    driver.find_element(By.CLASS_NAME,"understand").click()
+    driver.find_element(By.CLASS_NAME,"upload").click()
+    #time.sleep(30)
+
+    #start reverse search
+    checkbox = "checkbox"
+    checkbox = WebDriverWait(driver, 60).until(
+        EC.visibility_of_any_elements_located((By.XPATH, f'//*[@type="{checkbox}"]'))
+    )
+    for i in checkbox:
+        i.click()
+
+    buttons = driver.find_elements(By.XPATH,f'//button[@data-v-a60bceaf=""]')
+    buttons[3].click()
+
+
+    #export result csv
+    try:
+        while(True):
+            
+            lines = WebDriverWait(driver, 100).until(
+                EC.presence_of_element_located((By.XPATH, f'//button[@data-v-5cd5e023=""]'))
+            )
+            lines.click()
+            export = 'download'
+            export = WebDriverWait(driver, 20).until(
+                EC.visibility_of_element_located((By.XPATH, f'//*[@alt="{export}"]'))
+            )
+            export.click()
+
+            filename = WebDriverWait(driver, 20).until(
+                EC.visibility_of_element_located((By.ID, "filename"))
+            )
+            filename.send_keys("results")
+
+            export_csv = driver.find_element(By.XPATH,f'//button[@data-v-bbba1abc=""]').click()
+
+            if WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.CLASS_NAME,"container limited-container"))).click():
+                driver.close()
+            upload_agian = WebDriverWait(driver, 120).until(
+                EC.visibility_of_element_located((By.XPATH, f'//button[@data-v-7e26e312=""]'))
+            )
+            upload_agian.click()
+
+            search_agian = WebDriverWait(driver, 30).until(
+                EC.visibility_of_any_elements_located((By.XPATH, f'//button[@data-v-a60bceaf=""]'))
+            )
+            search_agian[3].click()
+    except (NoSuchWindowException,TimeoutException,WebDriverException) :
+        st.write(":: exception Occured ::")
+        driver.close()
+
     pass
 
 ## extract para----------------------------------------------------------------------------------
@@ -150,9 +227,6 @@ if extract_face_btn:
             dlinks.append(f'data:file/jpg;base64,{img_b64}')
         st.write(dlinks)
         time.sleep(5)
-        for i in dlinks:
-            res = rev_search(i)
-            st.write(res)
         
         #scrape and save the data
 scrap_form = st.form(key="scrapping")
